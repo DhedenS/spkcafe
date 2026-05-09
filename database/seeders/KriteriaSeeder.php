9860c256b4f3f48f38c162ba67cd3b98e37fc1fc
Sipoboy <alexrohmatullah1@gmail.com>
2026-05-09 18:23:03 +0700

    fix kriteria seeder

--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -6,38 +7,41 @@ use Illuminate\Support\Facades\DB;
 
 class KriteriaSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
         DB::table('tbl_kriteria')->insert([
             [
                 'id_kriteria' => 'C1',
                 'nama_kriteria' => 'Suasana',
-                'bobot' => 5,
+                'bobot' => 3,
                 'tipe' => 'benefit',
             ],
             [
                 'id_kriteria' => 'C2',
-                'nama_kriteria' => 'Luas Parkiran',
-                'bobot' => 2,
-                'tipe' => 'benefit',
+                'nama_kriteria' => 'Harga',
+                'bobot' => 3,
+                'tipe' => 'cost',
             ],
             [
                 'id_kriteria' => 'C3',
-                'nama_kriteria' => 'Harga',
-                'bobot' => 4,
+                'nama_kriteria' => 'Jarak',
+                'bobot' => 3,
                 'tipe' => 'cost',
             ],
             [
                 'id_kriteria' => 'C4',
-                'nama_kriteria' => 'Kecepatan Wifi',
-                'bobot' => 4,
+                'nama_kriteria' => 'Parkiran',
+                'bobot' => 3,
                 'tipe' => 'benefit',
             ],
             [
                 'id_kriteria' => 'C5',
-                'nama_kriteria' => 'Jarak',
+                'nama_kriteria' => 'Wifi',
                 'bobot' => 3,
-                'tipe' => 'cost',
+                'tipe' => 'benefit',
             ],
         ]);
     }
